e6a255797106f964c1adcb1495f0c1dc648c7df1
taroYxx <446734081@qq.com>
2020-10-10 16:18:36 +0800

    有效数独完成

--- leetCode_36_有效的数独.js
+++ leetCode_36_有效的数独.js
@@ -1,5 +1,48 @@
 var isValidSudoku = function(board) {
-    
+    let rows = [];
+    let columns = [];
+    let boxs = [];
+    for (let i = 0; i < 9; i++) {
+        rows.push([]);
+        columns.push([]);
+        boxs.push([]);
+    }
+
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let s = board[i][j];
+            if(s != "."){
+                // 行判断
+                if(rows[i].indexOf(s) == -1){
+                    rows[i].push(s);
+                }else{
+                    return false;
+                };
+
+                // 列判断
+                if(columns[j].indexOf(s) == -1){
+                    columns[j].push(s)
+                }else{
+                    return false;
+                }
+
+                // 九宫格判断
+                const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3); // 对应的盒子
+                if(boxs[boxIndex].indexOf(s) == -1){
+                    boxs[boxIndex].push(s)
+                }else{
+                    return false
+                }
+            }
+
+            
+        }
+        
+    }
+
+    return true;
+
 
 };
 
