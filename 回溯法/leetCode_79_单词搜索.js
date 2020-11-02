fdb4e18af8bd99b33c59cba94ee4e5ec4eb48991
taroYxx <446734081@qq.com>
2020-11-02 20:05:46 +0800

    举证回溯法

--- 回溯法/leetCode_79_单词搜索.js
+++ 回溯法/leetCode_79_单词搜索.js
@@ -1,31 +1,41 @@
 var exist = function(board, word) {
-    
-    let tag = false;
     let m = board.length;
     let n = board[0].length;
-    let res = [];
+    let visit = new Array(m);
+    for (let i = 0; i < visit.length; i++) {
+        visit[i] = new Array(n)
+    }
+    function dfs(x, y, i){
+        // 结果
+        if (i > word.length - 1) {
+            return true;
+        }
+        // 边界
+        if (x >= m || x < 0 || y >= n || y < 0) {
+            return false;
+        }
+        // 访问过的 以及 本来就不符合条件
+        if (visit[x][y] == true || board[x][y] != word[i]){
+            return false;
+        }
 
-    function dfs(cur, i, j){
-        if(cur.join("") == word){
-            tag = true;
-            return
+        // 开始访问 循环体
+        visit[x][y] = true;
+        if (dfs(x+1, y, i+1) || dfs(x-1, y, i+1) || dfs(x, y+1, i+1) || dfs(x, y-1, i+1)) {
+            return true;
         }
-        if(i == m || j== n){
-            return
-        }
-        for (let p = 0; p < board.length; p++) {
-            for (let q = 0; q < board[p].length; q++) {
-                console.log(cur);
-                cur.push(board[p][q])
-                dfs(cur,p+1,q);
-                cur.pop();
-            }
+
+        visit[x][y] = false;
+        return false;
+    }
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if(board[i][j] == word[0] && dfs(i, j, 0)){
+                return true;
+            }   
         }
     }
-
-    dfs([], 0, 0);
-
-    return res
+    return false
 
 };
 
