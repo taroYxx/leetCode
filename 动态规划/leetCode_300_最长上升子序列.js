531f4743bf2ff6125d757c44e4302876351fe186
taroYxx <446734081@qq.com>
2020-11-10 19:31:11 +0800

    子序列

--- 动态规划/leetCode_300_最长上升子序列.js
+++ 动态规划/leetCode_300_最长上升子序列.js
@@ -1,11 +1,22 @@
 var lengthOfLIS = function(nums) {
-    let tssass
+    let dp = new Array(nums.length).fill(1);
+    
+    dp[0] = 0;
+    dp[1] = 1;
 
-    for (var i = 1; dp[i] = 1, i < nums.length; i++)
-        for (var j = 0; j < i; j++)
-            nums[i] > nums[j] && (dp[i] = Math.max(dp[i], dp[j] + 1))
-
+    for (let i = 1; i < nums.length; i++) {
+        let j = i - 1;
+        let cur = nums[j]
+        while(j>=0){
+            if(cur<nums[i]){
+                dp[i] = dp[i] + 1;
+                cur = nums[j];
+            }
+            j--;
+        }
+    }
     return dp;
+
 };
 
 console.log(lengthOfLIS([4,10,4,3,8,9]))
