021ce35d035022174254ab7f763a4814595ec173
taroYxx <446734081@qq.com>
2020-11-24 11:18:31 +0800

    翻转二叉树

--- 二叉树!!!/leetCode_224_翻转二叉树.js
+++ 二叉树!!!/leetCode_224_翻转二叉树.js
@@ -5,7 +5,13 @@ function TreeNode(val) {
 
 
 var invertTree = function(root) {
-
+    if(root == null) return null;
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root
 };
 
 console.log(invertTree())
